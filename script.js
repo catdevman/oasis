df87806fe1387024fd0713fb9c832e320a111abf
Lucas Pearson <catdevman@gmail.com>
2026-01-09 20:44:16 -0500

    up vus and call either route in k6, keep next-plugins

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ import { sleep } from 'k6';
 
 export const options = {
   // Simulate 10 virtual users
-  vus: 250,
+  vus: 2500,
   // Run the test for a duration of 30 seconds
   duration: '30s',
 };
 
 export default function () {
   // Make a GET request to the target URL
-  http.get('http://127.0.0.1:8080/api/common/attendance');
+  if (Math.random() < 0.5){
+    http.get('http://127.0.0.1:8080/api/common/attendance');
+  } else {
+    http.get('http://127.0.0.1:8080/api/common/grades');
+  }
   // Add a small delay between requests to be more realistic
   sleep(1);
 }
